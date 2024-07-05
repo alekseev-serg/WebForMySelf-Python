4070336fc397da5c3de1b3a0ce30d1d658c7e987
sergey.alekseev <demiurg58@gmail.com>
2024-07-05 16:49:37 +0300

    add lesson8-string

---
+++ lesson8-string.py
@@ -0,0 +1,27 @@
+words = 'Hello World!'
+wordsAny = "Hello World!"
+print(words)
+print(wordsAny)
+
+wordsMore = "Hello, 'World'!"
+wordsMoreAny = 'Hello, "World"!'
+print(wordsMore)
+print(wordsMoreAny)
+
+wordsMoreMore = "Hello, \"test\" 'world'"
+wordsMoreMoreMore = "Hello, \test\" 'world'"
+print(wordsMoreMore)
+print(wordsMoreMoreMore)
+
+verse = """
+    Hello World!
+    Hello, 'World'!
+    Hello, "World"!
+"""
+print(verse)
+
+verseAny = 'Hello, "World"!\n\
+Hello, "World"!\n\
+Hello, "World"!'
+
+print(verseAny)
