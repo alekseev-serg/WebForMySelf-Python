5b7f1c4f60a8c3d37df63974646c91deaf71112e
sergey.alekseev <demiurg58@gmail.com>
2024-07-05 10:02:46 +0300

    add lessons6 vars

---
+++ lesson6-vars.py
@@ -0,0 +1,22 @@
+x = 1
+y = 3
+my_var1 = 50
+_myvar1 = 100
+# 1myvar = 200 - недопустимое имя.
+
+myVar = 30
+
+z = 3.5
+
+print(type(my_var1))
+print(type(y))
+print(type(z))
+
+X = 2
+
+print(x, X)
+
+TEST = 20
+print(TEST)
+TEST = 40
+print(TEST)
