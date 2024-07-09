a51a5246b1dbed9187bf6842344ec67fc91ed95d
sergey.alekseev <demiurg58@gmail.com>
2024-07-09 21:41:36 +0300

    do home work lesson 21

--- lesson21-homework-tuple.py
+++ lesson21-homework-tuple.py
@@ -8,3 +8,28 @@
 
 words = ['мадам', 'топот', 'тест', 'madam', 'word']
 my_str = ['Око за око', 'А роза упала на лапу азора', 'Около Миши молоко']
+
+new_words = []
+print(words[0][::-1])
+for item in words:
+    if item == item[::-1]:
+        new_words.append(item)
+print(new_words)
+
+print([item for item in words if item == item[::-1]])
+
+new_my_str = []
+for i in my_str:
+    if i.replace(' ', '').lower() == i.replace(' ', '').lower()[::-1]:
+        new_my_str.append(i)
+
+print([i for i in my_str if i.replace(' ', '').lower() == i.replace(' ', '').lower()[::-1]])
+
+tire = '-'
+new_string = tire.join(words)
+print(new_string)
+
+intList = list(range(1, 11))
+print(intList)
+s = '-'.join(map(str, intList))
+print(s)
