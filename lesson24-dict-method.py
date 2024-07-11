a6f560a8b82f9ae27c3aac51e1ad501dce5be7fc
sergey.alekseev <demiurg58@gmail.com>
2024-07-11 09:55:09 +0300

    add lesson 24-25

--- lesson24-dict-method.py
+++ lesson24-dict-method.py
@@ -1,1 +1,27 @@
 # Методы словаря
+product1 = {'title': 'Sony',
+            'price': 5.99
+            }
+product2 = dict(title='iPhone', price=5.99)
+
+# clear - очищает словарь
+
+print(product1.items())
+print(product2.keys())
+print(product2.values())
+print(product2.pop('title'))
+# print(product2.pop('title1'))
+print(product2.pop('title1', 'NO'))
+
+print(product2.popitem())
+
+print(product1)
+print(product1.setdefault('price', 5.99))
+print(product1)
+print(product1.setdefault('price4', 5.99))
+print(product1)
+product1.update({'test': 'value'})
+product1.update({'price': '250'})
+product1.update({'test3': 'value'})
+print(product1)
+
