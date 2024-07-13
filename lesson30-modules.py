e2be2a003c6e74dff36c2039b31609bc9226c526
sergey.alekseev <demiurg58@gmail.com>
2024-07-13 18:16:49 +0300

    add lesson 30 modules

--- lesson30-modules.py
+++ lesson30-modules.py
@@ -1,1 +1,26 @@
 # Модули
+import os
+import random
+import random as r
+
+print(os.getcwd())
+print(random.randint(0, 100))
+print(r.randint(0, 100))
+
+from random import randint, shuffle
+
+print(randint(0, 100))
+
+l = [1, 2, 3, 4, 5]
+shuffle(l)
+print(l)
+
+from modules import libs
+
+print(libs.get_length('Hello World'))
+print(libs.get_count('Hello World', 'e'))
+
+f = libs.get_count
+l1 = libs.get_length
+print(f('Hello World', 'H'))
+print(l1('Hi!'))
---
+++ modules/libs.py
@@ -0,0 +1,17 @@
+def get_count(string, symbol):
+    cnt = 0
+    for i in string:
+        if i == symbol:
+            cnt += 1
+    return cnt
+
+
+def get_length(string):
+    cnt = 0
+    for i in string:
+        cnt += 1
+    return cnt
+
+
+if __name__ == '__main__':
+    print(__name__)
