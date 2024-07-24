e5c488f2406a03f243d82b6615be079d4377f3ed
sergey.alekseev <demiurg58@gmail.com>
2024-07-24 21:48:45 +0300

    add lesson34-parsing

---
+++ lesson34-parsing.py
@@ -0,0 +1,30 @@
+# https://www.ua-football.com/sport
+
+from bs4 import BeautifulSoup
+import urllib.request
+
+url = 'https://www.ua-football.com/sport'
+request = urllib.request.urlopen(url)
+html = request.read()
+
+soup = BeautifulSoup(html, 'html.parser')
+news = soup.find_all('li', class_='liga-news-item')
+
+result = []
+for item in news:
+    title = item.find('span', class_='d-block').get_text(strip=True)
+    description = item.find('span', class_='name-dop').get_text(strip=True)
+    href = item.find('a').get('href')
+    result.append({
+        'title': title,
+        'description': description,
+        'href': href
+    })
+
+print(result)
+
+f = open('news.txt', 'w', encoding='utf-8')
+i = 1
+for item in result:
+    f.write(f'News №{i}\n\nНазвание: {item["title"]}\nОписание: {item["description"]}\nСсылка: {item["href"]}\n\n********\n')
+    i += 1
