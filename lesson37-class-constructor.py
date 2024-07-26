6ab7c655b28c98c844b74757a6058869c42948ff
sergey.alekseev <demiurg58@gmail.com>
2024-07-26 13:09:15 +0300

    add lesson37 class constructor

---
+++ lesson37-class-constructor.py
@@ -0,0 +1,7 @@
+from modules.classes import Person
+
+person1 = Person('Ivan', 23, 'male')
+person1.print_info()
+
+person2 = Person('Katy', 24, 'male')
+person2.print_info()
---
+++ modules/classes.py
@@ -0,0 +1,10 @@
+class Person:
+    # name = 'John'
+
+    def __init__(self, name, age, sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+
+    def print_info(self):
+        print(f'Hello, {self.name}')
